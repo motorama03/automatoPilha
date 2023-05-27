b521308e2c1510bd94999b6fb0aef7412199956f
Mateus Brandl <61717267+motorama03@users.noreply.github.com>
2023-05-26 22:25:52 -0300

    Add files via upload

---
+++ AutomatoPilha/src/automatoPilha/Nodo.java
@@ -0,0 +1,46 @@
+package automatoPilha;
+
+public class Nodo {
+	
+	private int dado;
+	private char elemento;
+	private Nodo prox;
+	
+	Nodo(int dado, char elemento){
+		this.dado = dado;
+		this.elemento = elemento;
+		prox = null;
+	}
+//	Nodo(int dado){
+//		this.dado = dado;
+//		prox = null;
+//	}
+
+	public int getDado() {
+		return dado;
+	}
+
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+
+	public char getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(char elemento) {
+		this.elemento = elemento;
+	}
+
+	public Nodo getProx() {
+		return prox;
+	}
+
+	public void setProx(Nodo prox) {
+		this.prox = prox;
+	}
+	
+	
+	
+	
+}
